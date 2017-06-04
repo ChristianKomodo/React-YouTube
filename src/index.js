ca295620b46c3ad0ec39526661b8ff74a6dde9ec
Christian Komodo <chris@sectorfive.com>
2017-06-04 15:11:52 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyDWWCP10Ej8qMYPTGrCOHieGsUC5YR062k';
+
+
+// Create a new component
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+// Render it
+ReactDOM.render(<App />, document.querySelector('.container'));
